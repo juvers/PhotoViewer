a19e7a8be5e152930900d01fdaa254e9908882a3
Jude kuti <judekuti@gmail.com>
2021-02-03 23:54:50 -0600

    Using ng-gallery and ng-lightbox

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Gallery, GalleryItem, ImageItem, ThumbnailsPosition, ImageSize } from 'ng-gallery';
+import { Lightbox } from 'ng-gallery/lightbox';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  items: GalleryItem[];
 
-  constructor() {}
+  imageData = data;
+  constructor(public gallery: Gallery, public lightbox: Lightbox) {}
+  ngOnInit() {
+
+    // Create gallery items
+    this.items = this.imageData.map(item => new ImageItem({ src: item.srcUrl, thumb: item.previewUrl }));
+
+    // Get a lightbox gallery ref
+    const lightboxRef = this.gallery.ref('lightbox');
+
+    // Add custom gallery config to the lightbox (optional)
+    lightboxRef.setConfig({
+      // imageSize: ImageSize.Cover,
+      thumbPosition: ThumbnailsPosition.Top
+    });
+
+    // Load items into the lightbox gallery ref
+    lightboxRef.load(this.items);
+  }
 
 }
+
+const data = [
+  {
+    srcUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg',
+    previewUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg'
+  },
+  {
+    srcUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg',
+    previewUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg'
+  },
+  {
+    srcUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg',
+    previewUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg'
+  },
+  {
+    srcUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg',
+    previewUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg'
+  }
+];
